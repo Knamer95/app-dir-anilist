02d900337f1cbb64515053f3b25f847e1f7eed62
Marcos <59671002+Knamer95@users.noreply.github.com>
2023-09-03 00:26:55 +0200

    feat: added remote host for AniList

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
