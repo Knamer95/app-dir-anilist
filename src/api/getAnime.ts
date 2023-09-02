70a2d00adeb902e8dea06cd6545b8e29a3748bc7
Marcos <59671002+Knamer95@users.noreply.github.com>
2023-09-03 00:50:22 +0200

    feat: updated to use Apollo

--- src/api/getAnime.ts
+++ src/api/getAnime.ts
@@ -1,39 +1,45 @@
+import { gql } from "@apollo/client";
+
+import { getClient } from "@/lib/client";
+import { AnimeQuery } from "./types";
+
+// TODO-mk - Change cache options, articles/docs for reference:
+// https://nextjs.org/docs/app/building-your-application/caching
+// https://www.apollographql.com/blog/apollo-client/next-js/how-to-use-apollo-client-with-next-js-13/
 export const getAnime = async (page: number, search?: string) => {
   const variables = {
     page,
     ...(search && { search }),
   };
 
-  const query = `
+  const query = gql`
     query ($page: Int, $search: String) {
-      Page (page: $page, perPage: 10) {
+      Page(page: $page, perPage: 10) {
         pageInfo {
           total
           currentPage
           lastPage
         }
-        media (type: ANIME, search: $search) {
+        media(type: ANIME, search: $search) {
           id
           title {
             romaji
             english
             native
           }
+          genres
+          episodes
+          coverImage {
+            large
+            color
+          }
+          meanScore
+          description
         }
       }
     }
   `;
 
-  const res = await fetch("https://graphql.anilist.co", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  });
-  return res.json();
+  const res = await getClient().query<AnimeQuery>({ query, variables });
+  return res.data;
 };
