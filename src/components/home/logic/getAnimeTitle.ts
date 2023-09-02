64d7c4353473e03e6c35493d1ebf0363982f5dbf
Marcos <59671002+Knamer95@users.noreply.github.com>
2023-09-03 00:49:24 +0200

    feat: added function to get the anime title

---
+++ src/components/home/logic/getAnimeTitle.ts
@@ -0,0 +1,17 @@
+import type { Anime, AnimeLanguage } from "@/api/types";
+
+const fallbackLanguage: AnimeLanguage = "native";
+
+/**
+ * @description Returns an anime title in the specified language
+ *
+ * @param title Object with the anime titles
+ * @param displayAnimeLanguage Chosen language
+ * @returns Title in the specified language, or the fallback language if the specified one was unavailable
+ */
+export const getAnimeTitle = (
+  title: Anime["title"],
+  displayAnimeLanguage: AnimeLanguage,
+) => {
+  return title[displayAnimeLanguage] ?? title[fallbackLanguage];
+};
