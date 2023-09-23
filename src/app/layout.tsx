2a6b3c8ebd19d64b6b2f75b15fd9567d231df2fb
Marcos <59671002+Knamer95@users.noreply.github.com>
2023-09-24 01:40:39 +0200

    feat: updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import "@mantine/core/styles.css";
 import { Inter } from "next/font/google";
 
+import { AnimeSearchInput } from "@/components/common/anime-search-input";
 import { ModeToggle } from "@/components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,20 +23,38 @@ type RootLayoutProps = {
 
 export default function RootLayout(props: RootLayoutProps) {
   const { children } = props;
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex items-center gap-x-2 bg-slate-900 p-3">
-            <p>Logo</p>
-            <Input className="w-64 flex-initial" />
-            <div className="flex-initial">
-              <ModeToggle />
-            </div>
-          </div>
+      <head>
+        <ColorSchemeScript />
+      </head>
 
-          <div className="content m-3">{children}</div>
-        </ThemeProvider>
+      <body className={inter.className}>
+        <MantineProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <header className="flex items-center gap-x-2 bg-slate-900 p-3 px-8">
+              <div className="grow">
+                <p className="w-fit cursor-pointer text-lg font-semibold">
+                  <Link href="/">Anime-R</Link>
+                </p>
+              </div>
+              <div className="flex items-center justify-items-end gap-4">
+                <AnimeSearchInput />
+                <div className="flex-initial">
+                  <ModeToggle />
+                </div>
+              </div>
+            </header>
+
+            <div className="m-2">
+              {/* Using a ref for the navbar height would require client component, so we use a fixed number */}
+              <ScrollArea className="relative h-[calc(100vh-60px-1rem)] w-full overflow-auto">
+                <div className="content m-6 p-4">{children}</div>
+              </ScrollArea>
+            </div>
+          </ThemeProvider>
+        </MantineProvider>
       </body>
     </html>
   );
